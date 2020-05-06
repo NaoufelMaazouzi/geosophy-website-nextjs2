6c35f9337b4c943c9a6888bc9e8e3b22aeb10877
NaoufelMaazouzi <naoufel.maazouzi@live.fr>
2020-05-06 17:29:14 +0200

    final emails files v2

--- pages/api/contact-route.js
+++ pages/api/contact-route.js
@@ -4,15 +4,10 @@ import nodemailer from "nodemailer"
 //const emailPass = "newworld28"
 
 const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com", // hostname
-    secureConnection: false, // TLS requires secureConnection to be false
-    port: 587, // port for secure SMTP
-    tls: {
-        ciphers: 'SSLv3'
-    },
+    service: "gmail",
     auth: {
-        user: 'naoufel.maazouzi@live.fr',
-        pass: 'newworld28'
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
     }
 })
 
