2c6e0b00c21347ebfa72fa28d9379928bc5cc40d
NaoufelMaazouzi <naoufel.maazouzi@live.fr>
2020-05-06 18:09:47 +0200

    change to port 587

--- pages/api/contact-route.js
+++ pages/api/contact-route.js
@@ -6,7 +6,7 @@ import nodemailer from "nodemailer"
 const transporter = nodemailer.createTransport({
     host: "smtp-mail.outlook.com", // hostname
     secureConnection: false, // TLS requires secureConnection to be false
-    port: 465, // port for secure SMTP
+    port: 587, // port for secure SMTP
     tls: {
         ciphers: 'SSLv3'
     },
@@ -30,7 +30,6 @@ const mailer = ({ mail, name, text, mobile, company }) => {
 
 
     const message = {
-        sender: from,
         to: 'naoufel.maazouzi@live.fr, naoufel_du_28@live.fr',
         subject: `Nouveau message de ${name} (provenant du site Geosophy)`,
         html: '<h4>Voici le message qui vous a été écrit: </h4>' + text + '<br>' + '<br><h4>Informations sur la personne: </h4><ul><li>Nom: ' + name + '</li><li>Adresse mail: ' + mail + '</li><li>Numéro de téléphone: ' + mobile + '</li><li>Companie: ' + company + '</li></ul>',
